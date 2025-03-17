caa79ef92e9d5693ca77be57e41bc588fcda7d06
Pandadulol <pandadulol.pro@gmail.com>
2025-03-17 12:25:42 +0100

    Working on Level Select in the grass, nearly finished

--- py scripts/main.py
+++ py scripts/main.py
@@ -13,7 +13,7 @@ screen = pygame.display.set_mode((640,480))
 pygame.display.set_caption('Ball Game')
 
 FPS = pygame.time.Clock()
-FPS.tick(5)
+FPS.tick(60)
 
 title_image = pygame.image.load(path+"Title.png")
 splash=pygame.image.load(path+"Putt_it_in.png")
@@ -32,9 +32,10 @@ forest_bg_levelselect=pygame.image.load("tout-savoir-sur-la-foret-1690461418.jpg
 desert_bg_levelselect=pygame.image.load("DATA_ART_11302929.JPG-1082602760.JPG")
 icesheet_bg_levelselect=pygame.image.load("cold-fish-441151-35585082.jpg")
 
+buttons=pygame.image.load(path+"Buttons.png")
 bckgroundgrass=pygame.image.load(path+"bckgroundgrass.png")
 
-scene = "Title"
+scene = "Forest"
 first_frame=True
 
 splashscale=1
@@ -47,14 +48,30 @@ desert_bg_levelselect_dim=(0,0,640,160)
 icesheet_bg_levelselect_dim=(0,0,640,160)
 bckgroundgrass_dim=(0,0,640,480)
 
+lvl1off=(0,96,48,48)
+lvl1on=(48,96,48,48)
+lvl2off=(96,96,48,48)
+lvl2on=(144,96,48,48)
+
+lvl3off=(0,144,48,48)
+lvl3on=(48,144,48,48)
+lvl4off=(96,144,48,48)
+lvl4on=(144,144,48,48)
+
+lvl5off=(0,144+48,48,48)
+lvl5on=(48,144+48,48,48)
+
 screen_center = screen.get_rect().center
+
+buttons = pygame.transform.scale(buttons, (3 * buttons.get_width(), 3 * buttons.get_height()))
+
 
 def on_button(gamestate,position,t_w,t_h):
     mouse_x, mouse_y = pygame.mouse.get_pos()
 
     if gamestate == "Title" and (position[0]+(t_w/2)>=int(mouse_x)>=position[0]-(t_w/2)) and (position[1]+(t_h/2)>=int(mouse_y)>=position[1]-(t_h/2)):
         return True
-    elif gamestate=="World Selection" and (position[0]<mouse_x<t_w) and (position[1]<mouse_y<t_h):
+    elif (position[0]<mouse_x<t_w) and (position[1]<mouse_y<t_h):
         return True
 
 while True:
@@ -88,10 +105,24 @@ while True:
             screen.blit(desert_bg_levelselect, desert_bg_levelselect.get_rect(center=(posx+180,posy+251)), desert_bg_levelselect_dim)
             screen.blit(icesheet_bg_levelselect, icesheet_bg_levelselect.get_rect(center=(posx+480,posy+708)), icesheet_bg_levelselect_dim)
             first_frame=False
+
     elif scene == "Forest":
         if first_frame:
             screen.fill((255, 255, 255))
             screen.blit(bckgroundgrass, bckgroundgrass.get_rect(center=(posx+3*640+320, posy+50)), bckgroundgrass_dim)
+
+            screen.blit(buttons, buttons.get_rect(center=(posx+270, posy+300)), lvl1off)
+            screen.blit(buttons, buttons.get_rect(center=(posx+130, posy+280)), lvl2off)
+            screen.blit(buttons, buttons.get_rect(center=(posx-120, posy+268)), lvl3off)
+            screen.blit(buttons, buttons.get_rect(center=(posx-60, posy+180)), lvl4off)
+            screen.blit(buttons, buttons.get_rect(center=(posx+290, posy+40)), lvl5off)
+
+            first_frame=False
+
+        if on_button(scene, (582,482), 598, 498):
+            screen.blit(buttons, buttons.get_rect(center=(posx+270, posy+300)), lvl1on)
+        print(pygame.mouse.get_pos())
+
 
     for event in pygame.event.get():
         if event.type == QUIT or (event.type == KEYDOWN and event.key == K_ESCAPE):
